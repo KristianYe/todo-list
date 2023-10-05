b79de830936c9d616c6237348cc39dee08f91764
Kristian Yendrek <kristianendreck4@gmail.com>
2023-10-05 12:25:43 +0300

    changed tag's status to is_done and set null=True for datetime_deadline

--- todo_app/models.py
+++ todo_app/models.py
@@ -4,10 +4,16 @@ from django.db import models
 class Task(models.Model):
     content = models.CharField(max_length=255)
     datetime_created_at = models.DateTimeField(auto_now_add=True)
-    datetime_deadline = models.DateTimeField()
-    status = models.BooleanField()
+    datetime_deadline = models.DateTimeField(null=True)
+    is_done = models.BooleanField()
     tags = models.ManyToManyField("Tag", related_name="tasks")
+
+    def __str__(self):
+        return f"{self.content}"
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name}"
