d2278d652d753cb949d8472a08a0bcc670b5ef71
Kristian Yendrek <kristianendreck4@gmail.com>
2023-10-05 11:09:35 +0300

    added Task, Tag models, added todo_app to settings, changed timezone

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    datetime_created_at = models.DateTimeField(auto_now_add=True)
+    datetime_deadline = models.DateTimeField()
+    status = models.BooleanField()
+    tags = models.ManyToManyField("Tag", related_name="tasks")
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
--- todo_list/settings.py
+++ todo_list/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "todo_app"
 ]
 
 MIDDLEWARE = [
@@ -106,11 +107,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
