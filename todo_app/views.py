d921c4e915d9281b38c0c27ef043bc875eeda212
Kristian Yendrek <kristianendreck4@gmail.com>
2023-10-05 12:49:25 +0300

    implemented TagListView

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.views import generic
 
-from todo_app.models import Task
+from todo_app.models import Task, Tag
 
 
 class TaskListView(generic.ListView):
     model = Task
     paginate_by = 5
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    paginate_by = 5
+
+
