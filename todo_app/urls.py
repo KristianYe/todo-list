cfb6068bfc7cac1effca31392d0b0acc0b5e22d5
Kristian Yendrek <kristianendreck4@gmail.com>
2023-10-05 13:31:53 +0300

    implemented tag create, update, delete views

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -5,7 +5,7 @@ from todo_app.views import (
     TagListView,
     TaskCreateView,
     TaskUpdateView,
-    TaskDeleteView
+    TaskDeleteView, TagCreateView, TagUpdateView, TagDeleteView
 )
 
 urlpatterns = [
@@ -14,6 +14,9 @@ urlpatterns = [
     path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tag/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tag/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tag/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "todo_app"
--- todo_app/views.py
+++ todo_app/views.py
@@ -36,3 +36,18 @@ class TagListView(generic.ListView):
     paginate_by = 5
 
 
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo_app:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo_app:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo_app:tag-list")
